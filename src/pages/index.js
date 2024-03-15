1b7690cbeea234fd7dd98258e99e97c33f279811
calebbabin <object.caleb@gmail.com>
2024-03-15 17:53:15 -0500

    basic nextjs setup

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+	return (
+		<main
+			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+		>
+			<Head>
+				<title>Personal Finance Calculator</title>
+			</Head>
+			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+				<div>
+					made by&nbsp;
+					<a
+						className="lg:pointer-events-auto text-blue-500 hover:underline"
+						href="https://calebbabin.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						caleb
+					</a>
+				</div>
+				<div>
+					view the&nbsp;
+					<a
+						className="lg:pointer-events-auto text-blue-500 hover:underline"
+						href="https://github.com/CalebBabin/personal-finance-calculator"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						source code
+					</a>
+				</div>
+			</div>
+		</main >
+	);
+}
